7fe9e0576a932f4d94d2d2ba2866b298c67ccba5
author)@ba0075bf-330a-0410-8768-ef3ec23a1e87 <author)@ba0075bf-330a-0410-8768-ef3ec23a1e87>
2009-03-01 17:05:42 +0000

    * Revamped header.rb as insert-header.rb
    git-svn-id: svn://localhost/subversion.repo/src/apache-license@544 ba0075bf-330a-0410-8768-ef3ec23a1e87

---
+++ insert-header.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+include FileUtils
+
+HDIR = File.dirname( __FILE__ )
+
+for fname in ARGV
+  if system( %q{egrep -q '^[ \*#]+Copyright \(C\)' } + fname  )
+    puts "#{fname} already has Copyright header."
+  else
+    hfile = HDIR + '/' +
+      if fname =~ /\.java$/ 
+        'header.java'
+      elsif fname =~ /\.xml$/ 
+        'header.xml'
+      else
+        'header.rb'
+      end
+    mv( fname, "#{fname}.orig" )
+    system( "cat #{hfile} #{fname}.orig > #{fname}" )
+    rm( "#{fname}.orig" )
+    puts "#{fname} : header applied."
+  end
+end
+
